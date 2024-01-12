fbb5ca259d325a3dd1efe497103ad134065f54a1
Prashant Jagtap <prashantjagtap6532@gmail.com>
2024-01-12 20:15:47 +0530

    added inheritance example

---
+++ Inheritance/inheritance-example8.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Human {
+    
+    private:
+    string name;
+    int age , weight;
+
+};
+
+class Student: protected Human{
+    private:
+    int roll , fee;
+
+
+   
+    public:
+    void fun(string n , int a , int w)
+    {
+        name = n;
+        age = a;
+        weight = w;
+    }
+    
+    void display()
+    {
+        cout<<name<<" "<<age<<" "<<weight<<" ";
+    }
+
+};
+   
+
+int main()
+{
+    Student A;
+    
+   
+    A.fun("Rohit" , 18 , 50);    //inaccessible
+    A.display();
+} 
---
+++ Inheritance/inheritance-example9.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Human {
+    
+    public:
+    string name;
+    int age , weight;
+
+};
+
+class Student: private Human{
+    private:
+    int roll , fee;
+
+
+   
+    public:
+    void fun(string n , int a , int w)
+    {
+        name = n;
+        age = a;
+        weight = w;
+    }
+    
+    void display()
+    {
+        cout<<name<<" "<<age<<" "<<weight<<" ";
+    }
+
+};
+   
+
+int main()
+{
+    Student A;
+    
+   
+    A.fun("Rohit" , 18 , 50);    //accessible
+    A.display();
+} 
