87ac8fcacb9afcc0c39aaa8e3abfc88bd4877903
Prashant Jagtap <prashantjagtap06532@gmail.com>
2024-01-14 20:45:16 +0530

    Hybrid inheritance added

---
+++ Types of inheritance/Hybrid-inheritance/Hybrid.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+
+class Student
+{
+    public:
+
+    void print()
+    {
+        cout<<"I am a student"<<endl;
+    }
+};
+
+class Male
+{
+    public:
+    void MalePrint()
+    {
+        cout<<"I am a Male"<<endl;
+    };
+};
+
+class Female
+{
+    public:
+    void FemalePrint()
+    {
+        cout<<"I am a Female"<<endl;
+    }
+};
+
+
+class Boy: public Student, public Male
+{
+    public:
+    void BoyPrint()
+    {
+        cout<<" I am a boy "<<endl;
+    };
+};
+class Girl: public Student, public Female
+{
+    public:
+    void GirlPrint()
+    {
+        cout<<" I am a girl "<<endl;
+    };
+};
+
+
+
+
+int main()
+{
+    Girl G1;
+    G1.GirlPrint();
+}
