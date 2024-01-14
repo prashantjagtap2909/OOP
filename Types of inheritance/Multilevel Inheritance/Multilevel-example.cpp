a706866a93ac9f147c0b822dd902a1e88df7207e
Prashant Jagtap <prashantjagtap06532@gmail.com>
2024-01-14 19:30:42 +0530

    multiple inheritance added

---
+++ Types of inheritance/Multilevel Inheritance/Multilevel-example.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class Person{
+    protected:
+    string name;
+
+    public:
+
+    void introduce(){
+        cout<<"Hello my name is "<<name<<endl;
+    }
+};
+
+class Employee: public Person
+{
+    protected:
+    int salary;
+
+    public:
+    void monthly_salary(){
+        cout<<"My monthly salary is "<<salary<<endl;
+    }
+};
+
+
+class Manager : public Employee{
+    
+    public:
+    string department;
+
+
+    Manager(string name , int salary , string department){
+        this->name = name;
+        this->salary = salary;
+        this->department = department;
+    };
+
+    void work(){
+        cout<<"I am leading the department of "<<department<<endl;
+    }
+
+
+};
+
+int main(){
+    Manager A1("Prashant" , 20000, "Finanace");
+    A1.work();
+}
